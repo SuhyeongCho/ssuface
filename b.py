a87a6284c3a62cf86ad95e2e87165a89efbd3bfb
SuhyeongCho <41462927+SuhyeongCho@users.noreply.github.com>
2019-02-17 00:05:01 +0900

    CNN structure

--- b.py
+++ b.py
@@ -15,7 +15,7 @@ pool_flat = tf.reshape(pool3,[-1,size*size*2])
 
 fc1 = tf.layers.dense(inputs=pool_flat,units=1024,activation=tf.nn.relu)
 fc2 = tf.layers.dense(inputs=fc1,units=256,activation=tf.nn.relu)
-output = tf.layers.dense(inputs=fc2,units=64)
+logits = tf.layers.dense(inputs=fc2,units=2)
 
 print(X.get_shape())
 print(conv1.get_shape())
@@ -27,16 +27,4 @@ print(pool3.get_shape())
 print(pool_flat.get_shape())
 print(fc1.get_shape())
 print(fc2.get_shape())
-print(output.get_shape())
-
-
-W = tf.Variable(tf.truncated_normal([64,2],stddev=1.0, dtype=tf.float32))
-b = b = tf.Variable(tf.random_normal([2]))
-
-print(W.get_shape())
-print(b.get_shape())
-
-logits = tf.matmul(output,W)+b
-
 print(logits.get_shape())
-
